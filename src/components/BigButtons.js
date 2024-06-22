4ae08392667f4667516672d293a79f75eb6cc81b
slizbethellis <slizbethellis@gmail.com>
2024-06-22 04:29:38 -0500

    added linktree-like links page for socials

---
+++ src/components/BigButtons.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default function BigButton ({ link, label, type }) {
+  const linkClasses = 'block w-full rounded-full text-center text-base font-semibold bg-gradient-to-r from-lila-800 to-lila-950 dark:from-fuzz-100 dark:to-fuzz-300 text-phthalo-50 dark:text-phthalo-950 hover:saturate-200 hover:no-underline px-8 py-4'
+
+  return (type === 'external' ?
+    (<a href={link} rel='noopener' className={linkClasses}>{label}</a>)
+    : (<Link to={link} className={linkClasses}>{label}</Link>))
+}
